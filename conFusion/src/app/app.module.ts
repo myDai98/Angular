bc7b99a3d4b4a9a7ddf118ff0a8419a50a689df5
Minyue Dai <daiminyue1998@gmail.com>
2017-10-30 13:55:54 -0400

    Form1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { FormsModule }   from '@angular/forms';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
@@ -20,8 +20,10 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
+
   declarations: [
     AppComponent,
     MenuComponent,
@@ -30,18 +32,27 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
+
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+
+  entryComponents: [
+        LoginComponent
+  ],
+
   providers: [DishService, 
     PromotionService,
     LeaderService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
